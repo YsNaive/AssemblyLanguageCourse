e21cde5ca788b23c96b794708d52da7bd6b53f56
天真 <yi.shi.4589@gmail.com>
2022-10-03 21:14:18 +0800

    week 3

--- ASM_CourseProject/lab/week 3/Lab3_65.asm
+++ ASM_CourseProject/lab/week 3/Lab3_65.asm
@@ -7,10 +7,15 @@ Result byte 9 DUP(?)
 Main PROC
 	Mov ecx, 9
 	Mov esi, OFFSET Result
-	
-calculate:
-	
+	MOV al, 9
 
+Calculate:
+	MOV [esi], al
+	ADD esi, 1
+	ADD al, 9
+	LOOP Calculate
+
+	MOV al, bl
 	exit
 main ENDP
 END main
